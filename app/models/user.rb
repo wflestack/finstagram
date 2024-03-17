f2e39b39d7d3f0ecfdf89ab20f605e0db7a2e835
David Weiss <d.weiss99@hotmail.com>
2024-03-17 21:26:39 +0000

    added sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ActiveRecord::Base
     has_many :likes
   
   end
+ 
+class User < ActiveRecord::Base
+
+    # ... The rest of your code is still here ...
+  
+    # --- Add these lines ---
+    validates :email, :username, uniqueness: true
+    validates :email, :avatar_url, :username, :password, presence: true
+    # -----------------------
+  
+  end
   
